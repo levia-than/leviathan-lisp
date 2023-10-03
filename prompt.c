264d61f96216e1528a1b213f829150acf817e5d4
levia-than <670916484@qq.com>
2023-10-03 14:16:23 +0800

    Motify the gitignore and fix bugs related to platform

--- prompt.c
+++ prompt.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 
 #include "prompt.h"
+// For Windows platform
+#ifdef _WIN32
+#include <string.h>
 // Buffer for User input.
 static char input[USER_INPUT_BUFFER];
+
+char *readline(char* prompt){
+  fputs (prompt, stdout);
+  fgets (buffer, USER_INPUT_BUFFER, stdin);
+  char *cpy = malloc (stdlen(buffer) + 1);
+  strcpy (cpy, buffer);
+  cpy (strlen(cpy) - 1) = '\0';
+  return cpy;
+}
+void add_history(char * unused){}
+
+// For Linux/Mac platform
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv){
 	// Print Basic information about the Lisp REPL
@@ -13,11 +32,12 @@ int main(int argc, char** argv){
   // Main loop
   while(1){
 		// Output the prompt
-      fputs ("lisp #", stdout);
-		// Read a line of user input into ouer buffer.
-      fgets (input, 2048, stdin);
+      char* input = readline("lisp #");
+      add_history (input);
+			
 		// Back print the result.
       printf ("%s", input);
+      free (input);
     }
   return 0;
 }
