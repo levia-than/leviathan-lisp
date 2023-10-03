b5e1c72bfbf18d42d7fd9ccb7c6278a87b3232b8
levia-than <670916484@qq.com>
2023-10-03 13:59:17 +0800

    initial skeleton for prompt file

--- prompt.c
+++ prompt.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-#include "prompt.h"
+#include <stdlib.h>
 
+#include "prompt.h"
 // Buffer for User input.
 static char input[USER_INPUT_BUFFER];
 
 int main(int argc, char** argv){
+	// Print Basic information about the Lisp REPL
+  puts ("Leviathan's Lisp Version 0.0.1");
+  puts ("Press Ctrl + c to Exit\n");
+
+  // Main loop
+  while(1){
+		// Output the prompt
+      fputs ("lisp #", stdout);
+		// Read a line of user input into ouer buffer.
+      fgets (input, 2048, stdin);
+		// Back print the result.
+      printf ("%s", input);
+    }
   return 0;
 }
